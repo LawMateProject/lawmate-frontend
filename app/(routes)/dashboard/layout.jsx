88c55bd1b7179fc942f2639347fad153e12dfd93
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-10-06 03:12:41 +0530

    feat: add side nav

---
+++ app/(routes)/dashboard/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SideNav from "./_components/SideNav";
+
+function DashboardLayout({ children }) {
+  return (
+    <div>
+      <div>
+        <SideNav />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
