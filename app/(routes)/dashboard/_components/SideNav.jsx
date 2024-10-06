c557e540681f7999da7f8a134e4f533c555d7f96
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-10-06 10:55:29 +0530

    feat: add styles to navbar

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   CalendarMinus2,
   Heart,
@@ -7,30 +8,35 @@ import {
   Scale,
   Tag,
 } from "lucide-react";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 
 function SideNav() {
-  // Group the first four under 'Menu' and the last three under 'Personal Navigator'
   const menuList = [
     {
       id: 1,
       name: "Community",
       icon: LayoutGrid,
+      path: "/dashboard",
     },
     {
       id: 2,
       name: "Tags",
+
       icon: Tag,
+      path: "/dashboard/tags",
     },
     {
       id: 3,
       name: "Appointments",
       icon: CalendarMinus2,
+      path: "/dashboard/appointments",
     },
     {
       id: 4,
       name: "My Lawyers",
       icon: Scale,
+      path: "/dashboard/myLawyers",
     },
   ];
 
@@ -39,30 +45,40 @@ function SideNav() {
       id: 5,
       name: "My Questions",
       icon: MessageCircleQuestion,
+      path: "/dashboard/myQuestions",
     },
     {
       id: 6,
       name: "My Answers",
       icon: MessageCircle,
+      path: "/dashboard/myAnswers",
     },
     {
       id: 7,
       name: "Likes & Votes",
       icon: Heart,
+      path: "/dashboard/likesVotes",
     },
   ];
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
 
   return (
-    <div className="h-screen p-5">
+    <div className="h-screen p-5 ">
       {/* Menu section */}
       <div className="mt-5">
-        <h3 className="text-sm  text-gray-500 mb-2 ml-4">MENU</h3>
+        <h3 className="text-sm text-gray-500 mb-2 ml-3">MENU</h3>
         {menuList.map((menu) => (
           <h2
             key={menu.id}
-            className="flex gap-4 items-center text-gray-700 text-sm p-4 cursor-pointer rounded-md"
+            className={`flex gap-4 items-center text-gray-700 text-sm p-3 cursor-pointer rounded-md hover:text-[#F48023] hover:bg-[#FCF4EC] hover:border-l-4 hover:border-[#F48023] transition-all ${
+              path == menu.path &&
+              "text-[#F48023] border-l-4 border-[#F48023] bg-[#FCF4EC]"
+            }`}
           >
-            <menu.icon />
+            <menu.icon className="text-gray-500" />
             {menu.name}
           </h2>
         ))}
@@ -70,13 +86,13 @@ function SideNav() {
 
       {/* Personal Navigator section */}
       <div className="mt-7">
-        <h3 className="text-sm  text-gray-500 mb-2 ml-4">PERSONAL NAVIGATOR</h3>
+        <h3 className="text-sm text-gray-500 mb-2 ml-3">PERSONAL NAVIGATOR</h3>
         {personalList.map((menu) => (
           <h2
             key={menu.id}
-            className="flex gap-4 items-center text-gray-700 text-sm p-4 cursor-pointer rounded-md"
+            className="flex gap-4 items-center text-gray-700 text-sm p-3 cursor-pointer rounded-md hover:text-[#F48023] hover:bg-[#FCF4EC] hover:border-l-4 hover:border-[#F48023] transition-all"
           >
-            <menu.icon />
+            <menu.icon className="text-gray-500" />
             {menu.name}
           </h2>
         ))}
